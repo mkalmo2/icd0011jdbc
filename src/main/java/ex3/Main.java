af14369730d0bd515a358698783b0eced62fb939
Märt <mart.kalmo@gmail.com>
2014-10-15 18:37:25 +0300

    starting state

--- src/main/java/ex3/Main.java
+++ src/main/java/ex3/Main.java
@@ -4,21 +4,11 @@ import java.sql.*;
 
 import org.apache.commons.dbutils.DbUtils;
 
-import ex3.dao.PersonDao;
-import ex3.dao.SetupDao;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-    	executeUpdate("DROP SCHEMA public CASCADE");
-        executeUpdate("CREATE TABLE person (name varchar(100))");
-        executeUpdate("Insert into person values ('Jack')");
-        executeUpdate("Insert into person values ('Jill')");
-        new PersonDao().printPersons();
-    }
 
-    private static void executeUpdate(String query) {
-		Connection conn = null;
+        Connection conn = null;
         Statement stmt = null;
         try {
             Class.forName("org.hsqldb.jdbcDriver");
@@ -26,13 +16,12 @@ public class Main {
                     "jdbc:hsqldb:file:${user.home}/data/jdbc/db;shutdown=true");
 
             stmt = conn.createStatement();
-            stmt.executeUpdate(query);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            stmt.executeUpdate("CREATE TABLE person (name varchar(100))");
         } finally {
             DbUtils.closeQuietly(stmt);
             DbUtils.closeQuietly(conn);
         }
-	}
+
+    }
 
 }
--- src/main/java/schema.sql
+++ src/main/java/schema.sql
@@ -1,3 +1,1 @@
 DROP SCHEMA public CASCADE;
-CREATE TABLE person (name varchar(100));
-Insert into person values ('Jack1');
