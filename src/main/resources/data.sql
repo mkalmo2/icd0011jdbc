41ddc47f54059e77a7d604cf30b1be06a78d8cf8
Märt Kalmo <mart.kalmo@gmail.com>
2018-09-28 10:31:46 +0300

    split schema.sql and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO PERSON (id, name, age)
+  VALUES (NEXT VALUE FOR seq1, 'Jane', 20);
+
+INSERT INTO PERSON (id, name, age)
+  VALUES (NEXT VALUE FOR seq1, 'John', 21);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,8 +7,3 @@ CREATE TABLE PERSON (
    name VARCHAR(255) NOT NULL,
    age int
 );
-
-INSERT INTO PERSON (id, name, age)
-  VALUES (NEXT VALUE FOR seq1, 'Jane', 20);
-INSERT INTO PERSON (id, name, age)
-  VALUES (NEXT VALUE FOR seq1, 'John', 21);
